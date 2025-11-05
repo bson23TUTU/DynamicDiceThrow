dafb8f48091a3e0b54ad3c7766846abda56faf7c
Brandon Son <tur48992@temple.edu>
2025-11-05 15:42:02 -0500

    TO DO 1 and 2 implemented in lab

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.lifecycle.ViewModelProvider
+import android.view.View
 
 /*
 Our DieThrow application has been refactored to move the dieRoll() logic
@@ -19,6 +20,10 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+
+    private var hasTwoColumns = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,6 +32,51 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show _only_ ButtonFragment if portrait
             - show _both_ fragments if Landscape
           */
+
+        val dieViewModel = ViewModelProvider(this)[DieViewModel::class.java]
+
+        hasTwoColumns = findViewById<View>(R.id.container2) != null
+
+        val buttonFragment = ButtonFragment()
+        val dieFragment = DieFragment()
+
+        if (savedInstanceState == null){
+            if (!hasTwoColumns) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container1, buttonFragment)
+                    .commit()
+            }
+        }
+        if (hasTwoColumns){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container1, buttonFragment)
+                .add(R.id.container2, dieFragment)
+                .commit()
+        }
+
+        dieViewModel.getDieRoll().observe(this){
+            if (!hasTwoColumns){
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1,dieFragment)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .commit()
+            }
+            if (hasTwoColumns){
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1,buttonFragment)
+                    .replace(R.id.container2,dieFragment)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .commit()
+
+            }
+        }
+
     }
 
     /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
